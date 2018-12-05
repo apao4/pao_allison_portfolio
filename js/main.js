1a20c094ca4a9d172a19abaebb5eff236ae5a4e0
Alli Pao <a_pao4@fanshaweonline.ca>
2018-12-05 04:46:56 -0500

    Fixed headers on all pages

--- js/main.js
+++ js/main.js
@@ -16,7 +16,18 @@ function myFunction() {
   }
 }
 
+// Active nav page
+var header = document.getElementById("navDes");
+var btns = header.getElementsByClassName("link");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
 
+//Resume
 var waypoint = new Waypoint({
   element:document.getElementById("profile"),
   handler:function(direction){
@@ -80,10 +91,29 @@ var waypoint = new Waypoint({
   offset: "80%"   
 })
 
+
+// Contact Form Pop Up
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
+}
+
+// Back to Top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
